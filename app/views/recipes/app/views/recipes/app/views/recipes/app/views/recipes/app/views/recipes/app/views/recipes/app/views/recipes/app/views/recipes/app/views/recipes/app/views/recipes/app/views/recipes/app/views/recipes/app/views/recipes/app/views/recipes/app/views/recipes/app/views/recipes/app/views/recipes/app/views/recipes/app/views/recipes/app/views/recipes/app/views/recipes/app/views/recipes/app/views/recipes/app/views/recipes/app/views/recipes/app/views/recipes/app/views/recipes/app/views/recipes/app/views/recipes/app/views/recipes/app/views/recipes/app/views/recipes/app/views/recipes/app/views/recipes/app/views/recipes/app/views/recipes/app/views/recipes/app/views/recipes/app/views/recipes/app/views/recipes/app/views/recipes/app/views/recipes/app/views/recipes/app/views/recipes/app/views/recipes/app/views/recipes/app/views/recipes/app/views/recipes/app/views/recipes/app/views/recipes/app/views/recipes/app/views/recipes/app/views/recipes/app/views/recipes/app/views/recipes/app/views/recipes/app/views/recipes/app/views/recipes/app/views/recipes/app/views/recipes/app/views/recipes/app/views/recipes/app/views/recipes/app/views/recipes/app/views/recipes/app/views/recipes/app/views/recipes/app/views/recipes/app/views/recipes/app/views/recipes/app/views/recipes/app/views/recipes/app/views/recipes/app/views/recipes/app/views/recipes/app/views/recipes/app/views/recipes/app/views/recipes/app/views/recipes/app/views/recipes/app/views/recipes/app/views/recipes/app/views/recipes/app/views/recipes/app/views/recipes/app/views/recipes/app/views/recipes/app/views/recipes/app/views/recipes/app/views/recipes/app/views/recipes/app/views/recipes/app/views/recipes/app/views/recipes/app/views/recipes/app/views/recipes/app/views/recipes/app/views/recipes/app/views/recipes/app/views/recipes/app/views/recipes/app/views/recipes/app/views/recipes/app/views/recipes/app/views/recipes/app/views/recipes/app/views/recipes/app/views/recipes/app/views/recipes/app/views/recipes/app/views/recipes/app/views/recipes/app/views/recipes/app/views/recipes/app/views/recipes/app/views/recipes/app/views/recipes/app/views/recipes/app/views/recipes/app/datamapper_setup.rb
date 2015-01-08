d98b7e450f84b807a6be56e0cfb871efac90c221
Elena <elena.15@hotmail.it>
2015-01-08 16:47:42 +0000

    removed app folder which was copied by mistake inside of models/recipes

--- app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/views/recipes/app/datamapper_setup.rb
+++
@@ -1,9 +0,0 @@
-env = ENV['RACK_ENV'] || 'development'
-DataMapper.setup(:default, "postgres://localhost/myrecipesjournal_#{env}")
-
-require_relative 'models/user.rb'
-require_relative 'models/recipe.rb'
-require_relative 'models/tag.rb'
-
-DataMapper.finalize
-DataMapper.auto_upgrade!
