34adfd4697dcb654b5c7bace533f48edacc57084
elena <elena.15@hotmail.it>
2015-01-11 20:09:53 +0000

    added a few lines of jquery and separated list of scripts

---
+++ public/js/application.js
@@ -0,0 +1,5 @@
+$( document ).ready(function() {
+  $(".sign_up_page").fadeIn(800)
+  $(".recipe_form ").fadeIn(800)
+	$('.input_text.search').on('click', function(){ $(this).effect( "shake", { distance: 10 } ) })
+});
