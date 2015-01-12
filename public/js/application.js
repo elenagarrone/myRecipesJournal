d47ec64f962a4b3e7b993530396d4e98921dd3bc
Elena <elena.15@hotmail.it>
2015-01-12 15:06:50 +0000

    fixed indentation in everything apart spec

--- public/js/application.js
+++ public/js/application.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
   $(".sign_up_page").fadeIn(800)
   $(".recipe_form ").fadeIn(800)
-	$('.input_text.search').on('click', function(){ $(this).effect( "shake", { distance: 10 } ) })
+  $('.input_text.search').on('click', function(){ $(this).effect( "shake", { distance: 10 } ) })
 });
